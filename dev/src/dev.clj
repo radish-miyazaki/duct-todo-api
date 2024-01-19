19e8af1b456771537385a74ab37965aaff135a05
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-01-19 22:32:43 +0900

    define db migrate & rollback func

--- dev/src/dev.clj
+++ dev/src/dev.clj
@@ -1,15 +1,19 @@
 (ns dev
   (:refer-clojure :exclude [test])
-  (:require [clojure.repl :refer :all]
-            [fipp.edn :refer [pprint]]
-            [clojure.tools.namespace.repl :refer [refresh]]
-            [clojure.java.io :as io]
-            [duct.core :as duct]
-            [duct.core.repl :as duct-repl :refer [auto-reset]]
-            [eftest.runner :as eftest]
-            [integrant.core :as ig]
-            [integrant.repl :refer [clear halt go init prep reset]]
-            [integrant.repl.state :refer [config system]]))
+  (:require
+   [clojure.java.io :as io]
+   [clojure.repl :refer :all]
+   [clojure.tools.namespace.repl :refer [refresh]]
+   [duct.core :as duct]
+   [duct.core.repl :as duct-repl :refer [auto-reset]]
+   [eftest.runner :as eftest]
+   [fipp.edn :refer [pprint]]
+   [integrant.core :as ig]
+   [integrant.repl :refer [clear go halt init prep reset]]
+   [integrant.repl.state :refer [config system]]
+   [ragtime.jdbc]
+   [ragtime.repl]))
+
 
 (duct/load-hierarchy)
 
@@ -18,6 +22,35 @@
 
 (defn test []
   (eftest/run-tests (eftest/find-tests "test")))
+
+(def ^:private env-profiles
+  {"dev" [:duct.profile/dev :duct.profile/local]})
+
+(defn- validate-env [env]
+  (when-not (some #{env} (keys env-profiles))
+    (throw (IllegalArgumentException. (format "env `%s` is undefined" env)))))
+
+(defn- load-migration-config [env]
+  (when-let [profiles (get env-profiles env)]
+    (let [prepped (duct/prep-config (read-config) profiles)
+          {{:keys [connection-uri]} :duct.database.sql/hikaricp} prepped
+          resources-key :duct.migrator.ragtime/resources]
+      {:datastore (ragtime.jdbc/sql-database connection-uri)
+       :migrations (-> prepped
+                       (ig/init [resources-key])
+                       (get resources-key))})))
+
+(defn db-migrate
+  "Migrate DB to the latest migration."
+  [env]
+  (validate-env env)
+  (ragtime.repl/migrate (load-migration-config env)))
+
+(defn db-rollback
+  "Rollback DB one migration."
+  [env]
+  (validate-env env)
+  (ragtime.repl/rollback (load-migration-config env)))
 
 (def profiles
   [:duct.profile/dev :duct.profile/local])
@@ -28,3 +61,4 @@
   (load "local"))
 
 (integrant.repl/set-prep! #(duct/prep-config (read-config) profiles))
+
