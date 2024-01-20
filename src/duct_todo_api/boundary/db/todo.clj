a2190bec9d2d717e3677c45297d8469279d04f5f
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-01-20 21:08:58 +0900

    define todo protocol & extend Todo + Boundary protocol

--- src/duct_todo_api/boundary/db/todo.clj
+++ src/duct_todo_api/boundary/db/todo.clj
@@ -1,7 +1,10 @@
 (ns duct-todo-api.boundary.db.todo
   (:require
    [clojure.spec.alpha :as s]
-   [duct.database.sql]))
+   [duct.database.sql]
+   [next.jdbc :as jdbc]
+   [next.jdbc.result-set :as rs]
+   [next.jdbc.sql :as sql]))
 
 (s/def ::id nat-int?)
 (s/def ::title string?)
@@ -33,4 +36,41 @@
   :args (s/cat :db any?
                :id ::id)
   :ret ::row-count)
+
+(defprotocol Todo
+  (find-todos [db])
+  (find-todo-by-id [db id])
+  (create-todo! [db todo])
+  (update-todo! [db id todo])
+  (delete-todo! [db id]))
+
+(defn ->connectable [db]
+  (-> db :spec :datasource))
+
+(def jdbc-opts
+  {:return-keys true
+   :builder-fn rs/as-unqualified-lower-maps})
+
+(extend-protocol Todo
+  duct.database.sql.Boundary
+  (find-todos [db]
+    (sql/query (->connectable db)
+               ["SELECT id, title FROM todos"] jdbc-opts))
+  (find-todo-by-id [db id]
+    (sql/get-by-id (->connectable db)
+                   :todos id jdbc-opts))
+  (create-todo! [db todo]
+    (-> (->connectable db)
+        (sql/insert! :todos (select-keys todo [:title]) jdbc-opts)))
+  (upsert-todo! [db id {:keys [title]}]
+    (-> (->connectable db)
+        (jdbc/execute-one! ["INSERT INTO todos (id, title) VALUES (?, ?)
+                               ON CONFLICT ON CONSTRAINT todos_pkey
+                               DO UPDATE SET title = ?"
+                            id title title]
+                           jdbc-opts)))
+  (delete-todo! [db id]
+    (-> (->connectable db)
+        (sql/delete! :todos {:id id})
+        :next.jdbc/update-count)))
 
