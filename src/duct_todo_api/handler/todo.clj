10f678bbe626dae8c3033281a5483d278ede6a34
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-01-21 13:25:07 +0900

    call boundary func

--- src/duct_todo_api/handler/todo.clj
+++ src/duct_todo_api/handler/todo.clj
@@ -1,43 +1,34 @@
 (ns duct-todo-api.handler.todo
   (:require
    [ataraxy.response :as response]
+   [duct-todo-api.boundary.db.todo :as db.todo]
    [integrant.core :as ig]))
 
-(def todos
-  (atom {1 {:id 1 :title "Build an API"}
-         2 {:id 2 :title "?????"}
-         3 {:id 3 :title "Profit!"}}))
-
-(defmethod ig/init-key ::list-todos [_ {:keys [_]}]
+(defmethod ig/init-key ::list-todos [_ {:keys [db]}]
   (fn [_]
     ;; TODO: DB Access
-    [::response/ok (vals @todos)]))
+    [::response/ok (db.todo/find-todos db)]))
 
-(defmethod ig/init-key ::create-todo [_ {:keys [_]}]
+(defmethod ig/init-key ::create-todo [_ {:keys [db]}]
   (fn [{[_ todo] :ataraxy/result}]
-    (let [todo-id (->> @todos
-                       keys
-                       (apply max)
-                       inc)]
-      (swap! todos assoc todo-id (merge {:id todo-id}
-                                        (select-keys todo [:title])))
-      [::response/created (str "/todos/" todo-id) (get @todos todo-id)])))
+    (let [todo-id (db.todo/create-todo! db todo)]
+      [::response/created (str "/todos/" todo-id) (db.todo/find-todo-by-id db todo-id)])))
 
-(defmethod ig/init-key ::fetch-todo [_ {:keys [_]}]
+(defmethod ig/init-key ::fetch-todo [_ {:keys [db]}]
   (fn [{[_ todo-id] :ataraxy/result}]
-    (if-let [todo (get @todos todo-id)]
+    (if-let [todo (db.todo/find-todo-by-id db todo-id)]
       [::response/ok todo]
       [::response/not-found {:message (str "Todo " todo-id " doesn't exist")}])))
 
-(defmethod ig/init-key ::delete-todo [_ {:keys [_]}]
+(defmethod ig/init-key ::delete-todo [_ {:keys [db]}]
   (fn [{[_ todo-id] :ataraxy/result}]
-    (if (get @todos todo-id)
-      (swap! todos dissoc todo-id)
+    (if (db.todo/find-todo-by-id db todo-id)
+      (do (db.todo/delete-todo! db todo-id)
+          [::response/no-content])
       [::response/not-found {:message (str "Todo " todo-id "doesn't exist")}])))
 
-(defmethod ig/init-key ::update-todo [_ {:keys [_]}]
+(defmethod ig/init-key ::update-todo [_ {:keys [db]}]
   (fn [{[_ todo-id todo] :ataraxy/result}]
-    (swap! todos assoc todo-id (merge {:id todo-id}
-                                      (select-keys todo [:title])))
-    [::response/created (str "/todos/" todo-id) (get @todos todo-id)]))
+    (db.todo/update-todo! db todo-id todo)
+    [::response/created (str "/todos/" todo-id) (db.todo/find-todo-by-id db todo-id)]))
 
